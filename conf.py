3f50943f005affde75c34098d666cb02c488fe3d
valentin.kozlov <valentin.kozlov@kit.edu>
2022-01-20 23:45:46 +0100

    [docs] Add section "How to use API (command line)" 	modified:   ../conf.py 	modified:   ../index.rst 	modified:   ../requirements.txt 	deleted:    guide.rst 	new file:   howto-api-cli.rst 	new file:   howto-jupyter-data.rst 	new file:   howto-jupyter-plot.rst 	new file:   tutorials.rst

--- conf.py
+++ conf.py
@@ -30,6 +30,7 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_copybutton',
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
@@ -37,7 +38,10 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.autosectionlabel',
-    'sphinxcontrib.openapi'
+    'sphinxcontrib.openapi',
+    'sphinxcontrib.youtube',
+    'sphinx-prompt',
+    'sphinx_substitution_extensions'
 ]
 autosectionlabel_prefix_document = True
 
@@ -68,6 +72,39 @@ html_logo = '_static/images/o3as-logo.svg'
 html_css_files = [
     'css/custom.css',
 ]
+
+# Force the "Edit on Github" button to use the configured URL
+gitlab_url = 'https://git.scc.kit.edu/synergy.o3as/o3docs'
+
+# A string of reStructuredText that will be included at the beginning of every source file that is read
+rst_prolog = """
+.. |o3api-v1-link| replace:: https://api.o3as.fedcloud.eu/api/v1/
+
+.. |o3-models-refc2| replace:: '[\n\
+   "CCMI-1_ACCESS_ACCESS-CCM-refC2",\n\
+   "CCMI-1_CCCma_CMAM-refC2",\n\
+   "CCMI-1_CESM1-CAM4Chem_refC2_r1i1p1",\n\
+   "CCMI-1_CESM1-CAM4Chem_refC2_r2i1p1",\n\
+   "CCMI-1_CESM1-CAM4Chem_refC2_r3i1p1",\n\
+   "CCMI-1_CESM1-WACCM_refC2_r1i1p1",\n\
+   "CCMI-1_CESM1-WACCM_refC2_r2i1p1",\n\
+   "CCMI-1_CESM1-WACCM_refC2_r3i1p1",\n\
+   "CCMI-1_CHASER-MIROC-ESM-refC2",\n\
+   "CCMI-1_CNRM-CERFACS_CNRM-CM5-3-refC2",\n\
+   "CCMI-1_CNRM-CERFACS_MOCAGE-refC2",\n\
+   "CCMI-1_ETH-PMOD_SOCOL3-refC2",\n\
+   "CCMI-1_GSFC_GEOSCCM-refC2",\n\
+   "CCMI-1_MESSy_EMAC-L90MA-refC2",\n\
+   "CCMI-1_MOHC_HadGEM3-ES-refC2",\n\
+   "CCMI-1_MRI_ESM1r1-refC2",\n\
+   "CCMI-1_NIES_CCSRNIES-MIROC3.2-refC2",\n\
+   "CCMI-1_NIWA_NIWA-UKCA-refC2",\n\
+   "CCMI-1_U-CAMBRIDGE_UMUKCA-UCAM-refC2",\n\
+   "CCMI-1_U-LAQUILA_CCM-refC2",\n\
+   "CCMI-1_U-LEEDS_UMSLIMCAT-refC2"\n\
+   ]'
+
+"""
 
 # A string of reStructuredText that will be included at the end of every source file that is read.
 # Add substitutions here that should be available in every file
---
+++ user/howto-jupyter-plot.rst
@@ -0,0 +1,2 @@
+How to retrieve a plot (jupyternotebook)
+==========================================
